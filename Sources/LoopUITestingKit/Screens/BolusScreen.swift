8f1ee35907627f015b9d2efd9f218470e9e21627
Foscottl-TP <scott@tidepool.org>
2025-02-26 23:10:20 +0900

    [QAE-456] New objects, CGM pill read func, and extension
    Changed bolusEntryTextField so it will support SImple Bolus Calulator and Bolus screens.
    Both Current Glucose and Bolus fields use the same identifier so rather than the default value determining the correct field, a supporting extension in Base Screen was added to help distinguish the the two.
    This can be used for parsing similar situations in future test cases.

--- Sources/LoopUITestingKit/Screens/BolusScreen.swift
+++ Sources/LoopUITestingKit/Screens/BolusScreen.swift
@@ -12,22 +12,44 @@ public final class BolusScreen: BaseScreen {
     // MARK: Elements
 
     private var bolusTitleText: XCUIElement { app.navigationBars.staticTexts["Bolus"] }
-    private var bolusEntryTextField: XCUIElement { app.textFields["dismissibleKeyboardTextField"] }
+    private var currentGlucoseEntryTextField: XCUIElement {
+        app.textFields.matching(NSPredicate(format: "identifier CONTAINS 'dismissibleKeyboardTextField'")).firstMatch
+    }
+    private var bolusEntryTextField: XCUIElement {
+        app.textFields.matching(NSPredicate(format: "identifier CONTAINS 'dismissibleKeyboardTextField'")).lastMatch
+    }
     private var bolusCancelButton: XCUIElement { app.navigationBars.buttons["Cancel"] }
     private var simpleBolusCalculatorTitle: XCUIElement { app.navigationBars.staticTexts["Simple Bolus Calculator"] }
     private var bolusActionButton: XCUIElement { app.buttons["button_bolusAction"] }
+    private var saveAndDeliverButton: XCUIElement { app.buttons["Save and Deliver"] }
     
     private var passcodeEntry: XCUIElement { springBoard.secureTextFields["Passcode field"] }
     private var keyboardDoneButton: XCUIElement { app.toolbars.firstMatch.buttons["Done"].firstMatch }
+    
+   
+    // Warnings
+    private var glucoseEntryRangeWarning: XCUIElement { app.staticTexts["Glucose Entry Out of Range"] }
+    
+    
+    
+    
     
     // MARK: Actions
     
     public var getBolusActionButtonLabel: String { bolusActionButton.getLableSafe() }
     
     public func tapCancelBolusButton() { bolusCancelButton.safeTap() }
+    public func tapCurrentGlucoseEntryTextField() {currentGlucoseEntryTextField.safeTap()}
     public func tapBolusEntryTextField() { bolusEntryTextField.safeTap() }
     public func tapBolusActionButton() { bolusActionButton.safeForceTap() }
+    public func tapSaveAndDeliverButton() {saveAndDeliverButton.safeForceTap()}
     
+    public func clearCurrentGlucoseEntryTextField() {
+        let currentTextLength = currentGlucoseEntryTextField.getValueSafe().count
+        
+        currentGlucoseEntryTextField
+            .typeText(String(repeating: XCUIKeyboardKey.delete.rawValue, count: currentTextLength))
+    }
     public func clearBolusEntryTextField() {
         let currentTextLength = bolusEntryTextField.getValueSafe().count
         
@@ -35,16 +57,20 @@ public final class BolusScreen: BaseScreen {
             .typeText(String(repeating: XCUIKeyboardKey.delete.rawValue, count: currentTextLength))
     }
     
+   
+    
     public func setPasscode() {
         passcodeEntry.safeTap()
         passcodeEntry.typeText("1\n")
     }
     
     public func setBolusEntryTextField(value: String) { bolusEntryTextField.typeText(value) }
+    public func setCurrentGlucoseEntryTextField(value: String) { currentGlucoseEntryTextField.typeText(value)}
     public func tapKeyboardDoneButton() { keyboardDoneButton.safeTap() }
     
     // MARK: Verifications
     
     public var bolusTitleExists: Bool { bolusTitleText.safeExists }
     public var simpleBolusCalculatorTitleExists: Bool { simpleBolusCalculatorTitle.safeExists }
+    public var glucoseEntryRangeWarningExists: Bool { glucoseEntryRangeWarning.safeExists }
 }
