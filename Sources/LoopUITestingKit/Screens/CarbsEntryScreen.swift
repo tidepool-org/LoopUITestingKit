e1fd2817e1ac91a872a4857b0d9f8f1f5250f34a
pragnasadhu <100800039+pragnasadhu@users.noreply.github.com>
2025-04-01 12:31:28 -0400

    Update carb entry page objects

--- Sources/LoopUITestingKit/Screens/CarbsEntryScreen.swift
+++ Sources/LoopUITestingKit/Screens/CarbsEntryScreen.swift
@@ -16,6 +16,14 @@ public final class CarbsEntryScreen: BaseScreen {
     private var carbEntryTitleText: XCUIElement { app.navigationBars.staticTexts["Add Carb Entry"] }
     private var carbsConsumedTextField: XCUIElement { app.textFields["textField_CarbsAmount"] }
     private var continueButton: XCUIElement { app.buttons["button_Continue"] }
+    private var mealBolusTitleText: XCUIElement { app.navigationBars.staticTexts["Meal Bolus"] }
+    private var bolusingText: XCUIElement { app.navigationBars.staticTexts["Bolused %1$@ of %2$@"] }
+
+    private var tapToStopActionButton: XCUIElement { app.buttons["Tap to Stop"] }
+
+    private var cancelingBolusText: XCUIElement { app.navigationBars.staticTexts["Canceling Bolus"] }
+
+    private var bolusCanceledText: XCUIElement { app.navigationBars.staticTexts["Bolus Canceled: Delivered %1$@ of %2$@"] }
     
     // MARK: Actions
     
@@ -38,9 +46,11 @@ public final class CarbsEntryScreen: BaseScreen {
         if emojiToSelect.isEmpty { XCTFail("Food type '\(foodType)' is not supported by test framework yet.") }
         app.staticTexts[emojiToSelect].safeTap()
     }
+     public func taptapToStopActionButton() { tapToStopActionButton.safeTap() }
     
     // MARK: Verifications
     
     public var simpleMealCalculatorExists: Bool { simpleMealCalculatorTitleText.safeExists }
     public var carbEntryScreenExists: Bool { carbEntryTitleText.safeExists }
+    public var mealBolusScreenExists: Bool { mealBolusTitleText.safeExists }
 }
