051cd683da8ce0bf20cf0b32cb0353cc9f81df7f
Foscottl-TP <scott@tidepool.org>
2025-05-08 13:44:39 -0700

    [QAE] Meal Bolus: Carb Entry + Health App
    -Added get Carb Entry count on Carbohydrates screen
    -Added Health App screens, Elements, Actions, and Verifications

---
+++ Sources/LoopUITestingKit/Screens/AppleHealthScreens.swift
@@ -0,0 +1,22 @@
+//
+//  AppleHealthScreens.swift
+//  LoopUITestingKit
+//
+//  Created by Scott Foster on 5/9/25.
+//
+
+import XCTest
+
+public final class AppleHealthScreens: BaseScreen {
+    
+    // MARK: Elements
+    
+    // MARK: Actions
+    
+    public func openHealthApp() {
+        XCUIApplication().launch()
+    }
+    
+    // MARK: Verifications
+    
+}
